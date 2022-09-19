4bf325c946f13026c95af3711fcd8ad6fb4ef5be
hsnadeeb <hsnadeeb007@gmail.com>
2022-09-19 16:08:16 +0530

    final todo list react app

---
+++ src/MyComponents/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <p>This is a todo list app created using react.js as my first task for my web development internship at Lets Grow More.</p>
+        </div>
+    )
+}
